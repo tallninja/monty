ab0dd226628816cd5a5e3a15d38ab4e6c4469657
Ernest Wambua <ernestwambua2@gmail.com>
2022-06-14 09:14:22 +0300

    _readfile

---
+++ _readfile.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+char *_readfile(char *filename, size_t bytes)
+{
+	int fd, buff_size = bytes;
+	char *buff;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (NULL);
+
+	buff = malloc(sizeof(char) * buff_size);
+	if (buff == NULL)
+		return (NULL);
+
+	bytes_read = read(fd, buff, buff_size);
+	if (bytes_read == -1)
+	{
+		free(buff);
+		return (NULL);
+	}
+
+	return (buff);
+}
--- monty.h
+++ monty.h
@@ -1,6 +1,10 @@
 #ifndef MONTY_H
 #define MONTY_H
 
+#include <stdlib.h>
+#include <stddef.h>
+#include <unistd.h>
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -31,5 +35,7 @@ typedef struct instruction_s
         char *opcode;
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+char *_readfile(char *filename, size_t bytes);
 
 #endif /* MONTY_H */
