d8a00a6407dd94dfa25ac164303039c720765853
Ernest Wambua <ernestwambua2@gmail.com>
2022-06-14 09:53:03 +0300

    _strcmp

--- _strcmp.c
+++ _strcmp.c
@@ -1,24 +1,22 @@
 #include "monty.h"
 
 /**
- * _strcmp - check equality of 2 strings
- * @s1: string 1
- * @s2: string 2
- * Return: 1 if the string are equal, 0 otherwise
+ * _strcmp - compares two strings
+ * @s1: first string to compare
+ * @s2: second string to compare
+ * Return: less than 0 if s1 is less than s2, 0 if they're equal,
+ * more than 0 if s1 is greater than s2
  */
-
 int _strcmp(char *s1, char *s2)
 {
 	unsigned int i = 0;
 
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (s1[i] == s2[i])
 	{
-		if (s1[i] != s2[i])
+		if (s1[i] == '\0')
 			return (0);
+
 		i++;
 	}
-	if (s1[i] != s2[1])
-		return (0);
-
-	return (1);
+	return (s1[i] - s2[i]);
 }
