0042922ca8f115b486396ddbf196028698c82c59
Ernest Wambua <ernestwambua2@gmail.com>
2022-06-15 19:13:49 +0300

    getopfunc

--- getopfunc.c
+++ getopfunc.c
@@ -1,24 +1,24 @@
 #include "monty.h"
 
 /**
- * _getopfunc - map the opcode to the function
+ * getopfunc - map the opcode to the function
  * @opcode: opcode string
  * Return: pointer to the opcode function
  */
 
-void (*_getopfunc(char *opcode))(stack_t**, unsigned int)
+void (*getopfunc(char *opcode))(stack_t**, unsigned int)
 {
 	int i = 0;
 	instruction_s ops[] = {
-		{"posh", push},
+		{"push", push},
 		{"pall", pall},
 		{NULL, NULL}
 	};
 
 	while (i < 2)
 	{
-		if (_strcmp(opcode, ops[i].opcode) == 0)
-			return (opd[i].f);
+		if (strcmp(opcode, ops[i].opcode) == 0)
+			return (ops[i].f);
 		i++;
 	}
 
